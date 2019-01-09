918d50c71e9c09716b329bd89b6db1a35120443f
Bruce Lai <bruceplai@gmail.com>
2019-01-08 23:15:46 -0800

    App module update

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  //needed to find pm-root directive
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule
   ],
+  //startup component
   bootstrap: [AppComponent]
 })
 export class AppModule { }
