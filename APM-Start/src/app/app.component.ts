cac348683068cf32704ce29ca0cf929141062ddf
Bruce Lai <bruceplai@gmail.com>
2019-01-09 17:36:06 -0800

    Product list component

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -4,11 +4,11 @@ import { Component } from '@angular/core'
   selector: 'pm-root',
   template: `
     <div><h1>{{pageTitle}}</h1>
-      <div>My First component</div>
+      <pm-products></pm-products>
     </div>
   `
 })
 
 export class AppComponent {
-  pageTitle: string = 'Acme Product Management'
+  pageTitle: string = 'BL Products'
 }
